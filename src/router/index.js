260303a1ea9986708174b67578bced0e684669c6
jydoskey <jideomotola33@gmail.com>
2022-06-20 13:38:21 +0100

    authentication router path created

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,7 @@ const router = createRouter({
       path: '/',
       name: 'signin',
       component: () => import('@/components/Authentication/signin.vue')
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: () => import('@/components/Authentication/signup.vue')
-    },
+    }
   ],
   scrollBehavior() {
     document.getElementById('app').scrollIntoView({ behavior: 'smooth' });
