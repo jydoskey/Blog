32fdba10421a91dcdb7408a888254e48f460e4cd
jydoskey <jideomotola33@gmail.com>
2022-07-24 10:02:33 +0100

    removed unused div

--- src/components/team.vue
+++ src/components/team.vue
@@ -1,39 +1,33 @@
 <template>
-  <div>
-    <div class="team">
-      <div class="leftmarker"></div>
-      <div>
-        <h2>{{ teamHeader }}</h2>
-        <p class="flex text-center pb-1">
-          <img
-            src="@/assets/images/calender.svg"
-            class="pr-2 w-5"
-            alt="calender"
-          />{{ teamDate }}
-        </p>
-        <p class="flex text-center">
-          <img src="@/assets/images/time.svg" class="pr-2 w-5" alt="time" />{{
-            teamTime
-          }}
-        </p>
-        <span class="flex mt-1">
-          <img
-            class="team-members"
-            src="@/assets/images/face1.png"
-            alt="face1"
-          />
-          <img
-            class="team-members -ml-2"
-            src="@/assets/images/face2.png"
-            alt="face2"
-          />
-          <img
-            class="team-members -ml-2"
-            src="@/assets/images/face3.png"
-            alt="face3"
-          />
-        </span>
-      </div>
+  <div class="team">
+    <div class="leftmarker"></div>
+    <div>
+      <h2>{{ teamHeader }}</h2>
+      <p class="flex text-center pb-1">
+        <img
+          src="@/assets/images/calender.svg"
+          class="pr-2 w-5"
+          alt="calender"
+        />{{ teamDate }}
+      </p>
+      <p class="flex text-center">
+        <img src="@/assets/images/time.svg" class="pr-2 w-5" alt="time" />{{
+          teamTime
+        }}
+      </p>
+      <span class="flex mt-1">
+        <img class="team-members" src="@/assets/images/face1.png" alt="face1" />
+        <img
+          class="team-members -ml-2"
+          src="@/assets/images/face2.png"
+          alt="face2"
+        />
+        <img
+          class="team-members -ml-2"
+          src="@/assets/images/face3.png"
+          alt="face3"
+        />
+      </span>
     </div>
   </div>
 </template>
